d52cbd5412fa158da94b678873851fbf263d5f1d
anggraeniastri <astrianggraeni22@yahoo.co.id>
2017-02-28 16:37:17 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+    public static void main (String argv[]){
+        System.out.println("Nama Lengkap : Astri Anggraeni");
+        System.out.println("NIM : 10115391");
+        System.out.println("Alamat : Jl Kubang Utara No.32 B");
+    }
+}
